12f92c9c6945fe6c7f9a6b08e3ec71aba074bb30
Sanghita chakraborty <sangchk714@gmail.com>
2024-07-04 21:04:46 +0530

    App set up.Addedlogin and reset password form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Entry from './Pages/entry/Entry.page';
+
+function App() {
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
