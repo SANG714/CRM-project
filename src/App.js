fa0c941ef286113201684b1ed367b56e8781a33d
Sanghita chakraborty <sangchk714@gmail.com>
2024-07-06 11:46:51 +0530

    Dashboard and Ticket Table added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 // import Entry from './Pages/entry/Entry.page';
 import DefaultLayout from './Layout/DefaultLayout';
+import Dashboard from './Pages/Dashboard/Dashboard.page';
 
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
       <DefaultLayout >
-        {/* Dashboard */}
+       <Dashboard/> 
       </DefaultLayout>
     </div>
   );
---
+++ src/Pages/New-ticket/AddTicket.page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+const AddTicket= () => {
+  return (
+    
+      <Container>
+        <Row>
+            <Col>
+            <PageBreadcrumb page= "New Ticket"/></Col>
+        </Row>
+        <Row>
+            <Col>
+            Form here</Col>
+        </Row>
+
+
+      </Container>
+    
+  )
+}
+
+export default AddTicket
