fc81fee2e08cf7ce9f3d01186ca64d2c19918544
Sanghita chakraborty <sangchk714@gmail.com>
2024-07-06 13:17:47 +0530

    Breadcrumbs

--- src/Pages/Dashboard/Dashboard.page.js
+++ src/Pages/Dashboard/Dashboard.page.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import TicketTable from '../../components/Ticket-table/TicketTable.comp';
 import tickets from "../../Assets/Data/Dummy-tickets.json"
+import PageBreadcrumb from '../../components/Breadcrumb/Breadcrumb.comp';
 
 const Dashboard = () => {
   return (
     <Container>
+        <Row>
+            <Col>
+            <PageBreadcrumb page="Dashboard"/>
+            </Col>
+        </Row>
         <Row>
             <Col className='text-center mt-5 mb-2'>
             <Button variant='info' style={{fontSize:"2rem", padding: "10px 30px"}}>Add New Ticket</Button>
